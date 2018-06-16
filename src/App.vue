fabf88af20b52b52c900359a3932aa33c3c8892d
curt <curt6815463@gmail.com>
2018-06-16 21:06:57 +0800

    add vue fontawesome

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <font-awesome-icon @click="test()" icon="spinner" />
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+export default {
+  name: 'App',
+  components: {
+    FontAwesomeIcon
+  },
+  computed: {
+  },
+  methods: {
+    // test(){
+    //   console.log('fuck');
+    // }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
